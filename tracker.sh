fdfc3a5b8a051d8d401b47a12d66c370400b6e6b
咯咯孤 <xiaoyi@163.com>
2023-06-06 02:05:25 +0800

    Update tracker.sh

--- tracker.sh
+++ tracker.sh
@@ -49,9 +49,9 @@ GET_TRACKERS() {
     if [[ -z "${CUSTOM_TRACKER_URL}" ]]; then
         echo && echo -e "$(DATE_TIME) ${INFO} Get BT trackers..."
         TRACKER=$(
-            ${DOWNLOADER} https://trackerslist.com/all_aria2.txt ||
+            ${DOWNLOADER} https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt ||
                 ${DOWNLOADER} https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt ||
-                ${DOWNLOADER} https://trackers.p3terx.com/all_aria2.txt
+                ${DOWNLOADER} https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt
         )
     else
         echo && echo -e "$(DATE_TIME) ${INFO} Get BT trackers from url(s):${CUSTOM_TRACKER_URL} ..."
